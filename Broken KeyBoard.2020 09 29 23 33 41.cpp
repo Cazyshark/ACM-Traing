96a3f4f678509b796e482c118c4da379f1d48b9d
Cazyshark <Cazyshark@gmail.com>
2020-09-30 12:29:09 +0800

    0930 12:29:

---
+++ Broken KeyBoard.2020 09 29 23 33 41.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 100005;
+int last,cur,next[maxn];
+char s[maxn];
+
+int main(){
+	while(scanf("%s",&s) == 1){
+		int n = strlen(s+1);
+		last = cur = 0;
+		next[0]=0;
+
+		for(int i =0; i<= n;i++){
+			char[]
+		}
+	}
+	return 0;
+}
