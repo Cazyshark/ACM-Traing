751db8e6aff10220c77c619b413c47417af951b1
Cazyshark <Cazyshark@gmail.com>
2020-09-30 13:17:33 +0800

    0930 13:17

---
+++ Dropping Balls.2020 09 30 12 40 13.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+const int maxd = 20;
+int s[1<<maxd];
+
+int main(){
+	long long d=pow(2,maxd)-1;
+	
+	printf("%ld\n",(1<<maxd));
+	return 0;
+}
