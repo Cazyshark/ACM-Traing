912afd52fdb2d5ad661af060b62bce634dad08b5
Cazyshark <Cazyshark@gmail.com>
2020-10-02 13:26:48 +0800

    1002 13:26

---
+++ sequence1~n.2020 10 02 13 21 38.cpp
@@ -0,0 +1,11 @@
+##########################################################################################
+#              void print_permutations(Sequence A,Set S){
+#                   if(S.empty()) out A;
+#                   else form  little to large in S.v
+#                   {
+#                        print_permutations(Sequence A+v,Set S-{v});
+#					}
+#			   }
+###########################################################################################
+
+
---
+++ sequence1~n.2020 10 02 13 25 38.cpp
@@ -0,0 +1,25 @@
+##########################################################################################
+#              void print_permutations(Sequence A,Set S){
+#                   if(S.empty()) out A;
+#                   else form  little to large in S.v
+#                   {
+#                        print_permutations(Sequence A+v,Set S-{v});
+#					}
+#			   }
+###########################################################################################
+
+
+void print_permutation(int n,int *A,int cur){
+	if(cur == n){
+		for(int i = 0;i < n;i++)printf("%d",A[i]);
+		printf("\n");
+	}
+	else for(int i = 1 ; i <= n; i++){
+		int ok  = 1;
+		for(int j = 0;j < cur;j++)
+				if(A[j] == i) ok = 0;
+		if(ok){
+			A[cur]
+		}
+	}
+}
